428598589ac54fe0abd3cc42842d58fae499d37f
GitHub Action <action@github.com>
2021-02-26 22:20:39 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/daidodo/formatimportsintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.daidodo.formatimportsintellij.services
+
+import com.github.daidodo.formatimportsintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/daidodo/formatimportsintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/daidodo/formatimportsintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.daidodo.formatimportsintellij.services
 
+import com.github.daidodo.formatimportsintellij.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
